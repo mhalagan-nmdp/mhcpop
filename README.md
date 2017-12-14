e7a4af20444ba374873a7e759a543cef464392fb
Mike Halagan <mhalagan@nmdp.org>
2017-12-14 15:03:52 -0600

    Updated LD.py to work with multiple frequency files and output to xlsx file. Updated README to match new code.

--- README.md
+++ README.md
@@ -12,5 +12,28 @@ pip install -r requirements.txt
 ## Example
 
 ```
-python bin/LD.py -f ExamplePop.freqs -o output/directory
+python bin/LD.py -f /path/to/frequencies -o /path/to/output/ -t ExampleTitle -v
+
+12/14/2017 02:57:49 PM - root - INFO - Loading /path/to/frequencies/ExamplePop1.freqs...
+12/14/2017 02:57:49 PM - root - INFO - Finished loading /path/to/frequencies/ExamplePop1.freqs
+12/14/2017 02:57:49 PM - root - INFO - 25629 unique haplotypes
+12/14/2017 02:57:54 PM - root - INFO - A~C       : # Haplos =   1212, Min D = -0.0102, Max D = 0.0231, # Significant =    759
+12/14/2017 02:57:54 PM - root - INFO - C~B       : # Haplos =    351, Min D = -0.0112, Max D = 0.0731, # Significant =    322
+12/14/2017 02:57:57 PM - root - INFO - B~DRB1    : # Haplos =   3456, Min D = -0.0046, Max D = 0.0259, # Significant =   2154
+12/14/2017 02:57:58 PM - root - INFO - DRB1~DQB1 : # Haplos =    288, Min D = -0.0210, Max D = 0.0895, # Significant =    256
+12/14/2017 02:57:58 PM - root - INFO - Finished ExamplePop1
+12/14/2017 02:57:58 PM - root - INFO - ---------------------------------------------------------------------------------------
+12/14/2017 02:57:58 PM - root - INFO - Loading /path/to/frequencies/ExamplePop2.freqs...
+12/14/2017 02:57:58 PM - root - INFO - Finished loading /path/to/frequencies/ExamplePop2.freqs
+12/14/2017 02:57:58 PM - root - INFO - 41639 unique haplotypes
+12/14/2017 02:58:07 PM - root - INFO - A~C       : # Haplos =   1465, Min D = -0.0103, Max D = 0.0185, # Significant =    865
+12/14/2017 02:58:07 PM - root - INFO - C~B       : # Haplos =    486, Min D = -0.0124, Max D = 0.0567, # Significant =    419
+12/14/2017 02:58:10 PM - root - INFO - B~DRB1    : # Haplos =   3993, Min D = -0.0048, Max D = 0.0212, # Significant =   2681
+12/14/2017 02:58:11 PM - root - INFO - DRB1~DQB1 : # Haplos =    417, Min D = -0.0238, Max D = 0.0884, # Significant =    356
+12/14/2017 02:58:11 PM - root - INFO - Finished ExamplePop2
+12/14/2017 02:58:11 PM - root - INFO - ---------------------------------------------------------------------------------------
+12/14/2017 02:59:17 PM - root - INFO - FINISHED ANALAYSIS
+12/14/2017 02:59:17 PM - root - INFO - Writing to /path/to/output/ExampleTitle.xlsx...
+
 ```
+
--- bin/LD.py
+++ bin/LD.py
@@ -1,7 +1,16 @@
 
+from itertools import repeat
 from scipy.stats import chi2
 import pandas as pd
 import argparse
+import logging
+import glob
+import sys
+import os
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.DEBUG)
 
 loci_order = {"A": 0, "C": 1, "B": 2, "DRB1": 3, "DRBX": 4,
               "DQA1": 5, "DQB1": 6, "DPA1": 7, "DPB1": 8}
@@ -31,7 +40,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--freq",
                         required=True,
-                        help="frequency file (ex. AAFA.freqs)",
+                        help="frequency directory",
                         type=str)
 
     parser.add_argument("-o", "--outdir",
@@ -39,81 +48,163 @@ def main():
                         help="Ouput directory",
                         type=str)
 
+    parser.add_argument("-t", "--title",
+                        required=True,
+                        help="Title of excel file",
+                        type=str)
+
     parser.add_argument("-v", "--verbose",
                         help="Option for running in verbose",
-                        default=False,
-                        type=bool)
+                        action='store_true')
 
     args = parser.parse_args()
-    data_file = args.freq
+
+    verbose = False
+    if args.verbose:
+        verbose = True
+
+    freqdir = args.freq
     outdir = args.outdir
-    df = pd.read_csv(data_file)
-    freq_name = data_file.split("/")[len(data_file.split("/"))-1].split(".")[0]
-    loci_df = df['Haplo'].apply(lambda x: pd.Series(x.split('~')))
-    loci = list(map(lambda x: x.split("*")[0], df['Haplo'][0].split("~")))
+    xlsx_name = outdir + "/" + args.title + ".xlsx"
+    freq_glob = freqdir + "/*.freqs"
 
-    loci_dic = {i: x for i, x in enumerate(loci)}
-    freq_df = loci_df.filter(loci_df.columns.values, axis=1)
+    # Check that frequency directory exists
+    if not os.path.isdir(freqdir):
+        logging.warning(freqdir + " is not a valid directory!")
+        parser.print_help()
+        sys.exit(2)
 
-    freq_df['Freq'], freq_df['Count'] = df['Freq'], df['Count']
-    freq_df = freq_df.rename(index=str,
-                             columns=loci_dic)
+    # Check that output directory exists
+    if not os.path.isdir(outdir):
+        logging.warning(outdir + " is not a valid directory!")
+        parser.print_help()
+        sys.exit(2)
 
-    sum_dic = dict(zip(loci, map(lambda loc:
-                                 freq_df.groupby([loc], as_index=True).sum()
-                                 .to_dict(orient="index"), loci)))
+    # Check that there are frequency files
+    if len(glob.glob(freq_glob)) < 1:
+        logging.warning("No frequency file found in " + freqdir)
+        parser.print_help()
+        sys.exit(2)
 
-    sorted_loci = sorted(loci, key=lambda x: loci_order[x])
-    loci_combos = list(map(lambda x: [sorted_loci[x], sorted_loci[x+1]],
-                           range(0, len(sorted_loci)-1)))
+    # Create excel file
+    writer = pd.ExcelWriter(xlsx_name, engine='xlsxwriter')
 
-    if 'A' in loci and 'B' in loci and ['A', 'B'] not in loci_combos:
-        loci_combos.append(['A', 'B'])
+    # Loop through frequency files
+    for f in glob.glob(freq_glob):
 
-    for loc_comb in loci_combos:
-        a, b = loc_comb[0], loc_comb[1]
-        ab_hap = "~".join(loc_comb)
-        a_sum = sum_dic[a]
-        b_sum = sum_dic[b]
-        ab_sum = freq_df.groupby(loc_comb, as_index=False).sum()
-        ab_df = ab_sum.apply(lambda row: pd.Series(["~".join([row[a], row[b]]),
-                                                    row['Count']]), axis=1)\
-                      .rename(index=str,
-                              columns={0: ab_hap, 1: "Count"})
-        ab = ab_df.groupby([ab_hap], as_index=True)\
-                  .sum().to_dict(orient="index")
-        ab_sum['D'] = ab_sum.apply(lambda row:
-                                   row['Freq'] -
-                                   (a_sum[row[a]]['Freq']*b_sum[row[b]]['Freq']),
-                                   axis=1)
-        ab_dict = ab_sum.groupby([a])[b].apply(list).to_dict()
-        ba_dict = ab_sum.groupby([b])[a].apply(list).to_dict()
-        total_count = freq_df['Count'].sum()
-        ab_df['p_val'] = ab_df.apply(lambda row: get_pval(row, ab_hap, ab,
-                                                          ab_dict,
-                                                          ba_dict,
-                                                          total_count),
-                                     axis=1)
-        ab_sum = ab_sum.apply(lambda row: pd.Series(["~".join([row[a],
-                                                               row[b]]),
-                                                    row['Freq'],
-                                                    row['Count'],
-                                                    row['D']]), axis=1)\
-                       .rename(index=str,
-                               columns={0: ab_hap, 1: "Freq", 2: "Count",
-                                        3: "D"})
-        ab_df['significant'] = ab_df.apply(lambda row:
-                                           '*' if row['p_val'] < .05 else '',
-                                           axis=1)
-        tmp = pd.merge(ab_df, ab_sum, on=ab_hap)
-        final_df = tmp.filter([ab_hap, 'Freq', 'Count_x', 'D', 'p_val',
-                               'significant'],
-                              axis=1)
-        final_df = final_df.rename(index=str,
-                                   columns={"Count_x": "Count"})
-        outfile = outdir + "/" + freq_name + "_" + "".join(loc_comb) + ".csv"
-        final_df.to_csv(outfile, index=False)
+        if verbose:
+            logging.info("Loading " + f + "...")
 
+        # Read freq file into pandas df
+        df = pd.read_csv(f)
+
+        if verbose:
+            logging.info("Finished loading " + f)
+            hapcnt = df['Haplo'].count()
+            logging.info(str(hapcnt) + " unique haplotypes")
+
+        # get population name
+        freq_name = f.split("/")[len(f.split("/"))-1].split(".")[0]
+
+        # get loci in frequency file
+        loci_df = df['Haplo'].apply(lambda x: pd.Series(x.split('~')))
+        loci = list(map(lambda x: x.split("*")[0], df['Haplo'][0].split("~")))
+
+        loci_dic = {i: x for i, x in enumerate(loci)}
+        freq_df = loci_df.filter(loci_df.columns.values, axis=1)
+
+        freq_df['Freq'], freq_df['Count'] = df['Freq'], df['Count']
+        freq_df = freq_df.rename(index=str,
+                                 columns=loci_dic)
+
+        sum_dic = dict(zip(loci, map(lambda loc:
+                                     freq_df.groupby([loc], as_index=True).sum()
+                                     .to_dict(orient="index"), loci)))
+
+        sorted_loci = sorted(loci, key=lambda x: loci_order[x])
+        loci_combos = list(map(lambda x: [sorted_loci[x], sorted_loci[x+1]],
+                               range(0, len(sorted_loci)-1)))
+
+        for loc_comb in loci_combos:
+            a, b = loc_comb[0], loc_comb[1]
+            ab_hap = "~".join(loc_comb)
+
+            a_sum = sum_dic[a]
+            b_sum = sum_dic[b]
+
+            # get all 2-loc haplotypes
+            ab_sum = freq_df.groupby(loc_comb, as_index=False).sum()
+            ab_df = ab_sum.apply(lambda row: pd.Series(["~".join([row[a],
+                                                        row[b]]),
+                                                        row['Count']]),
+                                 axis=1)\
+                          .rename(index=str,
+                                  columns={0: ab_hap, 1: "Count"})
+
+            # sum the counts across all 2-loc haplotypes
+            ab = ab_df.groupby([ab_hap], as_index=True)\
+                      .sum().to_dict(orient="index")
+
+            # Calculate LD
+            ab_sum['D'] = ab_sum.apply(lambda row:
+                                       row['Freq'] -
+                                       (a_sum[row[a]]['Freq']*b_sum[row[b]]['Freq']),
+                                       axis=1)
+            dmin = min(ab_sum['D'])
+            dmax = max(ab_sum['D'])
+            nhaps = ab_sum['D'].count()
+
+            # Calculate LD prime (D')
+            ab_sum['D\''] = ab_sum.apply(lambda row:
+                                            row['D'] / dmax if row['D'] > 0 else row['D'] / dmin,
+                                            axis=1)
+            ab_dict = ab_sum.groupby([a])[b].apply(list).to_dict()
+            ba_dict = ab_sum.groupby([b])[a].apply(list).to_dict()
+            total_count = freq_df['Count'].sum()
+
+            # Calculate p_val of LD
+            ab_df['p_val'] = ab_df.apply(lambda row: get_pval(row, ab_hap, ab,
+                                                              ab_dict,
+                                                              ba_dict,
+                                                              total_count),
+                                         axis=1)
+            ab_sum = ab_sum.apply(lambda row: pd.Series(["~".join([row[a],
+                                                                   row[b]]),
+                                                         row['Freq'],
+                                                         row['Count'],
+                                                         row['D'],
+                                                         row['D\'']]),
+                                  axis=1)\
+                           .rename(index=str,
+                                   columns={0: ab_hap, 1: "Freq", 2: "Count",
+                                            3: "D", 4: "D\'"})
+            ab_df['significant'] = ab_df.apply(lambda row:
+                                               '*' if row['p_val'] < .05 else '',
+                                               axis=1)
+            sign_cnt = ab_df[ab_df['significant'] == "*"]['significant']\
+                .count()
+
+            if verbose:
+                log ="{:10}: # Haplos = {:6d}, Min D = {:.4f}, Max D = {:.4f}, # Significant = {:6d}".format(ab_hap, nhaps, dmin, dmax, sign_cnt)
+                logging.info(log)
+
+            tmp = pd.merge(ab_df, ab_sum, on=ab_hap)
+            final_df = tmp.filter([ab_hap, 'Freq', 'Count_x', 'D',
+                                   'D\'', 'p_val',
+                                  'significant'],
+                                  axis=1)
+            final_df = final_df.rename(index=str,
+                                       columns={"Count_x": "Count"})
+            sheet = freq_name + "_LD-" + "".join(loc_comb)
+            final_df.to_excel(writer, sheet_name=sheet, index=False)
+
+        if verbose:
+            logging.info("Finished " + freq_name)
+            logging.info("".join(repeat("-", 110)))
+    if verbose:
+        logging.info("FINISHED ANALAYSIS")
+        logging.info("Writing to " + xlsx_name)
+    writer.save()
 
 if __name__ == '__main__':
     """The following will be run if file is executed directly,
